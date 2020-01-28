95843f83d2000e0fba54361946ccfdd0d401debd
Logan Sorensen <loganmsorensen@gmail.com>
2020-01-28 14:41:54 -0800

    can add new friend to friends array

--- friends/src/components/friends-list.jsx
+++ friends/src/components/friends-list.jsx
@@ -5,7 +5,12 @@ import Friend from "./friend";
 
 class FriendsList extends React.Component {
   state = {
-    friends: []
+    friends: [],
+    newFriend: {
+      name: "",
+      age: "",
+      email: ""
+    }
   };
 
   componentDidMount() {
@@ -15,17 +20,67 @@ class FriendsList extends React.Component {
   getData = () => {
     axiosWithAuth()
       .get("/api/friends")
-      .then(res =>
-        //res.data
-        this.setState({friends: res.data}),
+      .then(
+        res =>
+          //res.data
+          this.setState({ friends: res.data })
         // console.log(res)
       )
       .catch(err => console.log(err));
   };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state.newFriend)
+    axiosWithAuth()
+      .post("/api/friends", 
+        // id: Date.now(),
+        this.state.newFriend,
+        // age: this.state.newFriend.age,
+        // email: this.state.newFriend.email
+      )
+      .then(res => console.log(res))
+      .catch(err => console.log(err.message));
+  };
+
+  handleChanges = e => {
+    this.setState({
+      newFriend: {
+        ...this.state.newFriend,
+        [e.target.name]: e.target.value
+      }
+    });
+    console.log(this.state.newFriend);
+  };
+
   render() {
     return (
       <div>
+        <form onSubmit={this.handleSubmit}>
+          <p>Add a New Friend</p>
+          <input
+            type="text"
+            name="name"
+            value={this.state.newFriend.name}
+            placeholder="Name"
+            onChange={this.handleChanges}
+          />
+          <input
+            type="text"
+            name="age"
+            value={this.state.newFriend.age}
+            placeholder="Age"
+            onChange={this.handleChanges}
+          />
+          <input
+            type="text"
+            name="email"
+            value={this.state.newFriend.email}
+            placeholder="Email"
+            onChange={this.handleChanges}
+          />
+          <button>Add Friend</button>
+        </form>
         {this.state.friends.map(friend => {
           return <Friend friend={friend} />;
         })}
